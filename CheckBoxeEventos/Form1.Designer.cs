04deae7c85cf80d0395a4c20450a038d6fc2e945
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-03-15 22:10:58 -0300

    Adicionar arquivos de projeto.

---
+++ CheckBoxeEventos/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CheckBoxeEventos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkMaca = new System.Windows.Forms.CheckBox();
+            this.chkPera = new System.Windows.Forms.CheckBox();
+            this.chkUva = new System.Windows.Forms.CheckBox();
+            this.chkAbacaxi = new System.Windows.Forms.CheckBox();
+            this.chkMorango = new System.Windows.Forms.CheckBox();
+            this.chkBanana = new System.Windows.Forms.CheckBox();
+            this.chkMamao = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEscolha = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkMaca
+            // 
+            this.chkMaca.AutoSize = true;
+            this.chkMaca.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkMaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMaca.Location = new System.Drawing.Point(61, 68);
+            this.chkMaca.Name = "chkMaca";
+            this.chkMaca.Size = new System.Drawing.Size(64, 20);
+            this.chkMaca.TabIndex = 0;
+            this.chkMaca.Text = "Maçã";
+            this.chkMaca.UseVisualStyleBackColor = true;
+            // 
+            // chkPera
+            // 
+            this.chkPera.AutoSize = true;
+            this.chkPera.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkPera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkPera.Location = new System.Drawing.Point(61, 92);
+            this.chkPera.Name = "chkPera";
+            this.chkPera.Size = new System.Drawing.Size(59, 20);
+            this.chkPera.TabIndex = 1;
+            this.chkPera.Text = "Pêra";
+            this.chkPera.UseVisualStyleBackColor = true;
+            // 
+            // chkUva
+            // 
+            this.chkUva.AutoSize = true;
+            this.chkUva.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkUva.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUva.Location = new System.Drawing.Point(61, 116);
+            this.chkUva.Name = "chkUva";
+            this.chkUva.Size = new System.Drawing.Size(54, 20);
+            this.chkUva.TabIndex = 2;
+            this.chkUva.Text = "Uva";
+            this.chkUva.UseVisualStyleBackColor = true;
+            // 
+            // chkAbacaxi
+            // 
+            this.chkAbacaxi.AutoSize = true;
+            this.chkAbacaxi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkAbacaxi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAbacaxi.Location = new System.Drawing.Point(61, 140);
+            this.chkAbacaxi.Name = "chkAbacaxi";
+            this.chkAbacaxi.Size = new System.Drawing.Size(82, 20);
+            this.chkAbacaxi.TabIndex = 3;
+            this.chkAbacaxi.Text = "Abacaxi";
+            this.chkAbacaxi.UseVisualStyleBackColor = true;
+            // 
+            // chkMorango
+            // 
+            this.chkMorango.AutoSize = true;
+            this.chkMorango.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkMorango.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMorango.Location = new System.Drawing.Point(61, 164);
+            this.chkMorango.Name = "chkMorango";
+            this.chkMorango.Size = new System.Drawing.Size(87, 20);
+            this.chkMorango.TabIndex = 4;
+            this.chkMorango.Text = "Morango";
+            this.chkMorango.UseVisualStyleBackColor = true;
+            // 
+            // chkBanana
+            // 
+            this.chkBanana.AutoSize = true;
+            this.chkBanana.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkBanana.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkBanana.Location = new System.Drawing.Point(61, 188);
+            this.chkBanana.Name = "chkBanana";
+            this.chkBanana.Size = new System.Drawing.Size(79, 20);
+            this.chkBanana.TabIndex = 5;
+            this.chkBanana.Text = "Banana";
+            this.chkBanana.UseVisualStyleBackColor = true;
+            // 
+            // chkMamao
+            // 
+            this.chkMamao.AutoSize = true;
+            this.chkMamao.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkMamao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkMamao.Location = new System.Drawing.Point(61, 212);
+            this.chkMamao.Name = "chkMamao";
+            this.chkMamao.Size = new System.Drawing.Size(77, 20);
+            this.chkMamao.TabIndex = 6;
+            this.chkMamao.Text = "Mamão";
+            this.chkMamao.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Escolha a Fruta:";
+            // 
+            // btnEscolha
+            // 
+            this.btnEscolha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEscolha.Location = new System.Drawing.Point(377, 28);
+            this.btnEscolha.Name = "btnEscolha";
+            this.btnEscolha.Size = new System.Drawing.Size(273, 23);
+            this.btnEscolha.TabIndex = 8;
+            this.btnEscolha.Text = "Escolha a Fruta ao lado e clique aqui";
+            this.btnEscolha.UseVisualStyleBackColor = true;
+            this.btnEscolha.Click += new System.EventHandler(this.btnEscolha_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 282);
+            this.Controls.Add(this.btnEscolha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkMamao);
+            this.Controls.Add(this.chkBanana);
+            this.Controls.Add(this.chkMorango);
+            this.Controls.Add(this.chkAbacaxi);
+            this.Controls.Add(this.chkUva);
+            this.Controls.Add(this.chkPera);
+            this.Controls.Add(this.chkMaca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkMaca;
+        private System.Windows.Forms.CheckBox chkPera;
+        private System.Windows.Forms.CheckBox chkUva;
+        private System.Windows.Forms.CheckBox chkAbacaxi;
+        private System.Windows.Forms.CheckBox chkMorango;
+        private System.Windows.Forms.CheckBox chkBanana;
+        private System.Windows.Forms.CheckBox chkMamao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEscolha;
+    }
+}
+
---
+++ CheckBoxeEventos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBoxeEventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscolha_Click(object sender, EventArgs e)
+        {
+            String txt = ""; // escrever na tela
+            if (chkMaca.Checked) //se  checkBoxMaçã estiver selecionado ele vai escrever na tela maçã e assim por diante
+            {
+                txt += chkMaca.Text + ", ";
+            }
+            if (chkPera.Checked) 
+            {
+                txt += chkPera.Text + ", ";
+            }
+            if (chkUva.Checked) 
+            {
+                txt += chkUva.Text + ", ";
+            }
+            if (chkAbacaxi.Checked) 
+            {
+                txt += chkAbacaxi.Text + ", ";
+            }
+            if (chkMorango.Checked)
+            {
+                txt += chkMorango.Text + ", ";
+            }
+            if (chkBanana.Checked)
+            {
+                txt += chkBanana.Text + ", ";
+            }
+            if (chkMamao.Checked)
+            {
+                txt += chkMamao.Text + ", ";
+            }
+            MessageBox.Show(" A fruta escolhida foi: " + txt, "A fruta escolhida foi..."); // exibe a mensagem e as frutas escolhidas
+        }
+    }
+}
